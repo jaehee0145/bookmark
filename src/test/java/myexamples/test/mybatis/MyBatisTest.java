ce03ab7580d9aad9fea943dc49e3d7812ee1926a
choiwono <treasure_b@naver.com>
2019-03-06 19:03:29 +0900

    # spring security 설정변경

--- src/test/java/myexamples/test/mybatis/MyBatisTest.java
+++ src/test/java/myexamples/test/mybatis/MyBatisTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/appconfig-root.xml"})
+@ContextConfiguration(locations = {"file:../../../../../main/webapp/WEB-INF/spring/appconfig-root.xml"})
 public class MyBatisTest {
     @Autowired
     private SqlSessionFactory sqlFactory;
